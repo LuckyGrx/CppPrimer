2f1bd19cb054b79f267ddb8528832893c7359c98
LuckyGan <18357154046@163.com>
2017-06-25 09:58:31 +0800

    add something new

--- ex09/ex9_47.cc
+++ ex09/ex9_47.cc
@@ -33,9 +33,37 @@ void find_first_of_letter(const string& str){
 		++pos;
 	}
 }
+
+void find_first_not_of_number(const string& str){
+	string letter;
+	for(char i='a',j='A';i<='z'&&j<='Z';++i,++j){
+		letter+=i;
+		letter+=j;
+	}
+	string::size_type pos=0;
+	while((pos=str.find_first_not_of(letter,pos))!=string::npos){
+		cout<<"found number at index:"<<pos
+			<<" element is "<<str[pos]<<endl;
+		++pos;
+	}
+}
+
+void find_first_not_of_letter(const string& str){
+	string numbers("012345678");
+	string::size_type pos=0;
+	while((pos=str.find_first_not_of(numbers,pos))!=string::npos){
+		cout<<"found letter at index:"<<pos
+			<<" element is "<<str[pos]<<endl;
+		++pos;
+	}
+}
 int main(){
 	string name("ab2c3d7R4E6");
+	cout<<"use find_first_of:"<<endl;
 	find_first_of_number(name);
 	find_first_of_letter(name);
+	cout<<"use_find_first_not_of:"<<endl;
+	find_first_not_of_number(name);
+	find_first_not_of_letter(name);
 	return 0;
 }
