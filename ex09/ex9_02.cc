2fe1643233dd00aa14a84e26ae1fbee175404320
LuckyGan <18357154046@163.com>
2017-06-29 16:48:51 +0800

    add some answers

---
+++ ex09/ex9_02.cc
@@ -0,0 +1,15 @@
+ ///
+ /// @file    ex9_02.cc
+ /// @author  zack(18357154046@163.com)
+ /// @date    2017-06-29 16:43:30
+ ///
+ 
+#include <list>
+#include <deque>
+using std::list;
+using std::deque;
+
+int main(){
+	list<deque<int>> ldi;
+	return 0;
+}
