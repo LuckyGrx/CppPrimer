52f009a9ab1c1ddbc0e25b9a9c8826110271ce1a
LuckyGan <18357154046@163.com>
2017-07-07 21:54:16 +0800

    20170707 third commit

---
+++ ex02/test.cc
@@ -0,0 +1,15 @@
+ ///
+ /// @file    test.cc
+ /// @author  zack(18357154046@163.com)
+ /// @date    2017-07-07 21:22:27
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(){
+	const int i2=-1;
+	const int& const r2;
+	return 0;
+}
