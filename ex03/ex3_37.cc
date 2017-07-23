708ba64344623ccc813aafe9f7fc8c0c0f0d1c98
LuckyGan <18357154046@163.com>
2017-07-23 22:04:48 +0800

    20170723 second commit

--- ex03/ex3_37.cc
+++ ex03/ex3_37.cc
@@ -7,13 +7,14 @@
 #include <iostream>
 using std::cout;
 using std::endl;
-
+//打印的结果是不确定的，因为判断条件×cp是以‘\0'为结束的，但
+//显然字符数组ca并没有包含字符'\0'，++cp会一直进行直到找到某个内存块
+//中存放的是'\0'为止
 int main(){
 	const char ca[]={'h','e','l','l','o'};
-	const char s[]="world";
 	const char* cp=ca;
 	while(*cp){
-		cout<<*cp<<endl;
+		cout<<*cp;
 		++cp;
 	}
 	return 0;
