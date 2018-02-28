cf93e3cbb8196e42d2cf09831e35c8e97fae0165
LuckyGan <15002364225@163.com>
2018-02-27 23:01:30 -0800

    20180228 third commit

--- ch01/ex1_08.cc
+++ ch01/ex1_08.cc
@@ -13,3 +13,10 @@ int main(){
     std::cout << /* "*/" /* "/*" */;
     return 0;
 }
+//
+// ex1_08.cc:12:24: warning: missing terminating " character
+//      std::cout << /* "*/" */;
+//
+// ex1_08.cc:12:5: error: missing terminating " character
+//      stdd::cout << /* "*/" */;
+//
